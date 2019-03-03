3fd0aa48ce4986feaad1ee7d64da92650173d7a8
Amol Jagtap <ajfsdeveloper@gmail.com>
2019-03-03 20:22:55 +0530

    Refactor using chain

--- src/actions/index.js
+++ src/actions/index.js
@@ -35,7 +35,12 @@ const fetchUser = id => async dispatch => {
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts())
 
-  const userIds = _.uniq(_.map(getState().posts, 'userId'))
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'))
+  // userIds.forEach(id => dispatch(fetchUser(id)))
 
-  userIds.forEach(id => dispatch(fetchUser(id)))
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value()
 }
