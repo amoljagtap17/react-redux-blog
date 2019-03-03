6c0c39ca30087eedd0f3e9441301dafa464c1d42
Amol Jagtap <ajfsdeveloper@gmail.com>
2019-03-03 15:44:20 +0530

    Using redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,18 @@
-export const fetchPosts = () => {
-  return {
-    type: 'FETCH_POSTS'
+import jsonPlaceHolder from '../apis/jsonPlaceHolder'
+
+/* export const fetchPosts = () => {
+  return async function(dispatch, getState) {
+    const response = await jsonPlaceHolder.get('/posts')
+
+    dispatch({
+      type: 'FETCH_POSTS',
+      payload: response
+    })
   }
+} */
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceHolder.get('/posts')
+
+  dispatch({ type: 'FETCH_POSTS', payload: response })
 }
